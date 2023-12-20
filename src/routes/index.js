f4afc22a98e9d75d5a9213d0166a1c2ade8bd1d7
Davi Marques <davi.giareta@nubbi.com.br>
2023-12-20 11:14:42 -0300

    ESLINT CONFIG

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,9 @@ import express from "express";
 import items from "./itemsRoutes.js";
 
 const routes = (app) => {
-    app.route("/").get((req,res) => res.status(200).send("Welcome to RESIDENT EVIL API 🧟"));
+  app.route("/").get((req,res) => res.status(200).send("Welcome to RESIDENT EVIL API 🧟"));
     
-    app.use(express.json(), items);
-}
+  app.use(express.json(), items);
+};
 
 export default routes;
